0b4bf543a1311c59cbf5fea9f5b90f5f23e5c42e
Chua Chong Herng <chong_herng@hotmail.com>
2021-10-18 03:54:43 +0800

    changed folder structure

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs')
-app.set("views", path.join(__dirname, "/../client/views"));
+app.set("views", path.join(__dirname, "/../public/views"));
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     console.log("Request for index.html");
-    res.sendFile(path.join(__dirname + "/../client/views/index.html"));
+    res.sendFile(path.join(__dirname + "/public/views/index.html"));
 })
 
 app.listen(3000);
