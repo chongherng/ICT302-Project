2f77df1bfd432d437a3a0ac7176fa012e8c38233
Chua Chong Herng <chong_herng@hotmail.com>
2021-10-18 12:19:24 +0800

    Added server route logging

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.use(logger);
+
 app.set("views", path.join(__dirname, "/../public/views"));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    console.log("Request for index.html");
+app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname + "/public/views/index.html"));
-})
+});
+
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/views/index.html"));
+});
+
+function logger(req, res, next) {
+    console.log("Request for " + req.originalUrl);
+    next();
+}
 
 app.listen(3000);
