570a5aee585719d5c612f532ecece59897d50d3a
Chua Chong Herng <chong_herng@hotmail.com>
2021-10-29 18:47:47 +0800

    Added routing for staff login and SSS request portal

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const routes = require('./routes/home');
-
+const home = require('./routes/home');
+const request = require('./routes/request');
 
 // View engine setup
 app.set("view engine", "ejs");
-app.use(logger);
+app.use(logger); // Comment out this logger if needed
 
 app.set("views", path.join(__dirname, "/../public/views"));
 app.use(express.static("public"));
 
-app.use('/', routes);
+app.use('/', home);
+app.use('/request', request);
+
 
 function logger(req, res, next) {
     console.log("Request for " + req.originalUrl);
---
+++ routes/request.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+// Get SSS request portal
+router.get("/Student-Support-Staff-Request-Portal.html", (req, res) => {
+  res.sendFile(path.join(__dirname,"../public/views/Student-Support-Staff-Request-Portal.html"));
+});
+
+// Get Student request portal
+
+module.exports = router;
